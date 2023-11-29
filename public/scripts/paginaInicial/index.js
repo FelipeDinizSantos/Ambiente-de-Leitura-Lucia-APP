4d2b0d97992bf7ce6d3fdf2e4f3fc67a1515aac7
Felipe Diniz dos Santos <felipedinizdossantos0@gmail.com>
2023-11-28 23:10:08 -0300

    Finalização da aplicação web

--- public/scripts/paginaInicial/index.js
+++ public/scripts/paginaInicial/index.js
@@ -1,9 +1,25 @@
 const submit = document.getElementById('submitBtn');
 const input = document.getElementById('inputSearch');
 
+function validateString(str) 
+{
+    var regex = /\s{2,}/;
+
+    if (regex.test(str)) return false;
+
+    return true;
+}
+
+  
+
 submit.addEventListener('click', ()=>
 {
-    window.location.assign('/buscar?search=' + input.value);
+
+    if(input.value != null && input.value != "" && validateString(input.value))
+    { 
+        window.location.assign('/buscar?search=' + input.value);
+    }
+    else alert('Preencha o campo de pesquisa!');
 })
 
 window.addEventListener('keypress', (event)=>
